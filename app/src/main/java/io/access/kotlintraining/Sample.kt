57bcd545edd496c1dee9e3dae6089a86303be221
Yukitoshi Sato <yukitoshi.sato@access-company.com>
2018-04-19 12:20:01 +0900

    閏年とtest追加

--- app/src/main/java/io/access/kotlintraining/Sample.kt
+++ app/src/main/java/io/access/kotlintraining/Sample.kt
@@ -1,5 +1,7 @@
 package io.access.kotlintraining
 
+import java.util.Scanner
+
 fun welcome() {
     println("welcome to ACCESS!")
 }
@@ -8,15 +10,32 @@ fun hello() {
     println("Hello World!")
 }
 
+
 fun fizzbuzz() {
     for (i in 1..100)
-        if (i % 15 == 0) {
-            println("FizzBuzz")
-        } else if (i % 3 == 0) {
-            println("Fizz")
-        } else if (i % 5 == 0) {
-            println("Buzz")
-        } else {
-            println(i)
+        when {
+            i % 15 == 0 -> println("FizzBuzz")
+            i % 3 == 0 -> println("Fizz")
+            i % 5 == 0 -> println("Buzz")
+            else -> println(i)
         }
+}
+
+fun inputyear() {
+    val input = Scanner(System.`in`)
+    println("年を入力")
+    var year : Int = input.nextInt()
+    isLeapYear(year)
+}
+
+
+fun isLeapYear(y: Int): Boolean {
+    if (y % 4 == 0) {
+        if (y % 100 == 0) {
+            return y % 400 == 0
+        }
+        return true
+    } else {
+        return false
+    }
 }
--- app/src/test/java/io/access/kotlintraining/SampleTest.kt
+++ app/src/test/java/io/access/kotlintraining/SampleTest.kt
@@ -1,6 +1,7 @@
 package io.access.kotlintraining
 
 import org.junit.Test
+import java.util.*
 import kotlin.test.assertEquals
 
 class SampleTest {
@@ -32,6 +33,15 @@ class SampleTest {
         val actual = 1
         assertEquals(expected, actual, "[want] $expected [got] $actual")
 
+    }@Test
+    fun testisLeapyear(){
+        assertEquals(true, isLeapYear(1600))
+        assertEquals(true, isLeapYear(2000))
+        assertEquals(true, isLeapYear(2400))
+        assertEquals(false, isLeapYear(1700))
+        assertEquals(false, isLeapYear(2100))
+        assertEquals(false, isLeapYear(2200))
+        assertEquals(false, isLeapYear(1999))
     }
 }
 
